087cf84812d0c5fd13164a32b8c315959b238130
Anurag Patil <aspanurag@gmail.com>
2020-11-07 08:56:59 -0500

    DailyByte| String|  isCapitalized

---
+++ DailyByte/4.py
@@ -0,0 +1,45 @@
+"""
+No: 4
+Date: 11-07-2020
+
+Problem:
+    isCapitalized
+    Given a string, return whether or not it uses capitalization correctly. 
+    A string correctly uses capitalization if all letters are capitalized, 
+    no letters are capitalized, or only the first letter is capitalized.
+
+TestCases:
+    Ex: Given the following strings...
+
+    "USA", return true
+    "Calvin", return true
+    "compUter", return false
+    "coding", return true
+
+Time Complexity:
+    
+Space Complexity:
+    
+"""
+
+
+def isCapitalized(str) -> bool:
+    if str == str.upper():
+        return True
+    if str == str[0].upper() + str[1:]:
+        return True
+    if str == str.lower():
+        return True
+    return False
+    pass
+
+
+tests = {
+    "USA": True,
+    "Calvin": True,
+    "compUter": False,
+    "coding": True,
+    "Padjkfnmclkl12 23kd adjk": True,
+}
+for test, result in tests.items():
+    assert isCapitalized(test) == result, test + " should be " + str(not result)
--- README.md
+++ README.md
@@ -13,6 +13,7 @@ Practice Coding Problems Daily
 1. 2020-11-04 | String | Given a string, reverse all of its characters and return the resulting string.
 2. 2020-11-05 | String | Given a string, check if palidrome
 3. 2020-11-06 | String | Sequence of Moves
+4. 2020-11-07 | String | isCapitalized
 
 ### DailyCodingProblem
 
