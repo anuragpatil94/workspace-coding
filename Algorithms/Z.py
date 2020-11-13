fca560b64949a5784d55d607bbd6bbc73f92fdc8
Anurag Patil <aspanurag@gmail.com>
2020-11-13 09:02:18 -0500

    Z comments

--- Algorithms/Z.py
+++ Algorithms/Z.py
@@ -3,17 +3,34 @@ def createZArray(text):
     left = right = 0
     current = 1
     while current < len(text):
+        # When working outside the Z window
         if current > right:
+            # since current is greater than right, we already finised the last
+            # search and starting the new search. Hence we move left and right
+            # to current and start new window
             left = right = current
             while right < len(text) and text[right] == text[right - current]:
                 right += 1
             zArr[current] = right - left
             right -= 1
+        # Working Inside the Z Window
         else:
+            # We are taking the previously calculated Z Indexes to check if we
+            # have a new match that goes beyond the right index. This confirms
+            # that next 'n' characters are a match and we would compare after
+            # these `n` characters since they don't have to be compared again
+            # which helps reducing number of comparisons.
+            # example:
+            # L----C----R => Cindex + Zindex[C-L]
+            # Why Zindex[C-L]? This will give xth letter index from pattern
+            # which is appended before text.
             if zArr[current - left] + current - 1 < right:
                 zArr[current] = zArr[current - left]
             else:
-                left = right = current
+                # Since we already have compared some indexes from current to
+                # right we don't have to compare again.
+                # ZWindow starts at C and we check right if it is match.
+                left = current
                 while right < len(text) and text[right] == text[right - current]:
                     right += 1
                 zArr[current] = right - left
